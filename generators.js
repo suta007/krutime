f4199a8bb841889e14683a78f63538fe205e28a7
afx007 <mr.suta@gmail.com>
2019-08-15 17:41:00 +0700

    Delete generators.js

--- generators.js
+++
@@ -1,6 +0,0 @@
-Blockly.JavaScript["KruTime.read"] = function(block) {
-    // TODO: Assemble JavaScript into code variable.
-    var code = 'DEV_I2C1.KRUTIME(0, 0x8).getRawValue()';
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.JavaScript.ORDER_NONE];
-  };
